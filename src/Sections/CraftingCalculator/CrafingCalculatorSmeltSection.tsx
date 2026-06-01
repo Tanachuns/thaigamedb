29210ac07f713a0dbd4bfa3155997d0fa84562be
Tanachuns <tanarak.chuns@gmail.com>
2026-06-01 17:09:39 +0700

    add cal with current mat

--- src/Sections/CraftingCalculator/CrafingCalculatorSmeltSection.tsx
+++ src/Sections/CraftingCalculator/CrafingCalculatorSmeltSection.tsx
@@ -3,16 +3,15 @@ import products from "../../assets/data/CraftingCalculator.json";
 import CraftingProductRadio from "../../Components/CraftingProductRadio";
 import { getImage } from "../../Helpers/ImageHelper";
 
-
 type Task = {
-  name:string;
+  name: string;
   source: string;
   times: number;
   amount: number;
 };
 
 type Result = {
-  product:string|undefined
+  product: string | undefined;
   amount: number;
   staminacost: number;
   tasks: Task[];
@@ -20,8 +19,8 @@ type Result = {
 
 export default function CrafingCalculatorSmeltSection() {
   const [result, setResult] = useState<Result>({
-    product:"",
-    amount:0,
+    product: "",
+    amount: 0,
     staminacost: 0,
     tasks: [],
   });
@@ -31,6 +30,10 @@ export default function CrafingCalculatorSmeltSection() {
     event.preventDefault();
     const productAmount: number = Number(event.target.product_amount.value);
     const smelt_product = event.target.smelt_product.value;
+    const cur_item = [
+      event.target.cur_item_a.value,
+      event.target.cur_item_b.value,
+    ];
 
     if (!smelt_product) {
       setErrorMsg("กรุณาเลือก Enchance Material");
@@ -43,22 +46,31 @@ export default function CrafingCalculatorSmeltSection() {
     }
 
     const _result: Result = {
-      product:smelt_product,
-      amount:productAmount,
+      product: smelt_product,
+      amount: productAmount,
       staminacost: 0,
       tasks: [],
     };
+
     // =(((mat_cost/yield)*gat_cost)+...)+cft_cost
     const product = products.find((f) => f.name === smelt_product);
-    product?.materials.forEach((m) => {
-      const gather = Math.ceil(m.mat_cost / m.yield) * productAmount;
+    product?.materials.forEach((m, i) => {
+      const gather = Math.ceil(
+        (m.mat_cost * productAmount - cur_item[i]) / m.yield,
+      );
       //console.log(m.title+": "+gather)
       _result.staminacost += gather * m.gat_cost;
       //console.log(m.title+" staminacost: "+ m.gat_cost)
-      _result.tasks.push({ name:m.title,source: m.source, times: gather,amount:m.mat_cost* productAmount});
+      _result.tasks.push({
+        name: m.title,
+        source: m.source,
+        times: gather,
+        amount: m.mat_cost * productAmount - cur_item[i],
+      });
     });
-      //console.log(" staminacost: "+ _result.staminacost)
-    _result.staminacost += (product?.cft_cost as number*productAmount) ;
+    //console.log(" staminacost: "+ _result.staminacost)
+    _result.staminacost += (product?.cft_cost as number) * productAmount;
+
     if (!_result.staminacost) {
       setErrorMsg("เกิดข้อผิดพลาด");
     }
@@ -66,14 +78,49 @@ export default function CrafingCalculatorSmeltSection() {
     setResult(_result);
   };
 
+  // const handleCurStemChanged = (
+  //   e: React.FocusEvent<HTMLInputElement, Element>,
+  // ) => {
+  //   const amount = document.getElementById(
+  //     "product_amount",
+  //   ) as HTMLInputElement;
+
+  //   const smelt_product = document.getElementById(
+  //     "smelt_product",
+  //   ) as HTMLInputElement;
+
+  //      const cur_item_a = document.getElementById(
+  //     "cur_item_a",
+  //   ) as HTMLInputElement;
+  //    const cur_item_b = document.getElementById(
+  //     "cur_item_b",
+  //   ) as HTMLInputElement;
+
+  //    const cur_item = [
+  //     cur_item_a.value,
+  //     cur_item_b.value,
+  //   ];
+
+  //   let staminacost = 0
+
+  //    const product = products.find((f) => f.name === smelt_product.value);
+  //   product?.materials.forEach((m, i) => {
+  //     const gather = Math.ceil(
+  //       (m.mat_cost - Number(cur_item[i]) ) / m.yield,
+  //     )
+  //     staminacost += gather * m.gat_cost;
+  //   });
+  //   amount.value =  (Number(e.target.value)-staminacost).toLocaleString();
+  // };
+
   const resetError = () => {
     setErrorMsg(null);
   };
 
   const resetForm = () => {
     setResult({
-      product:"",
-      amount:0,
+      product: "",
+      amount: 0,
       staminacost: 0,
       tasks: [],
     });
@@ -83,10 +130,53 @@ export default function CrafingCalculatorSmeltSection() {
       <form onSubmit={(e) => calHandler(e)}>
         <h2 className="text-xl">Enchance Material</h2>
         <div className="grid gap-2 grid-cols-4 w-fit">
-          {products.map((p,i)=>{
-            return <CraftingProductRadio key={i} image={getImage(p.name+".png")} inputName="smelt_product" inputValue={p.name} name={p.title} resetError={resetError}  />
+          {products.map((p, i) => {
+            return (
+              <CraftingProductRadio
+                key={i}
+                image={getImage(p.name + ".png")}
+                inputName="smelt_product"
+                inputValue={p.name}
+                name={p.title}
+                resetError={resetError}
+              />
+            );
           })}
         </div>
+        <details
+          className="collapse bg-base-100 border border-base-300 p-2 w-fit"
+          name="my-accordion-det-1"
+        >
+          <summary>คำนวนเพิ่ม</summary>
+          {/* <label className="input">
+            <input
+              type="number"
+              onBlur={(e) => handleCurStemChanged(e)}
+              className="grow"
+              placeholder="stamina ปัจจุบัน"
+              name="cur_stamina"
+            />
+          </label> */}
+          <label className="input mt-1">
+            <input
+              type="number"
+              className="grow"
+              placeholder="จำนวนไอเทมฝั่งซ้าย"
+              name="cur_item_a"
+              id="cur_item_a"
+
+            />
+          </label>
+          <label className="input mt-1">
+            <input
+              type="number"
+              className="grow"
+              placeholder="จำนวนไอเทมฝั่งขวา"
+              name="cur_item_b"
+              id="cur_item_b"
+            />
+          </label>
+        </details>
         จำนวน
         <div>
           <input
@@ -95,6 +185,7 @@ export default function CrafingCalculatorSmeltSection() {
             defaultValue={1}
             name="product_amount"
             className="input input-primary w-25 text-center "
+            id="product_amount"
           />
         </div>
         <button type="submit" className="mt-1 btn btn-warning">
@@ -111,15 +202,25 @@ export default function CrafingCalculatorSmeltSection() {
       </form>
       <div className="divider"></div>
       <b className="text-xl">ผลลัพธ์</b>
-      <p>{result.product} {result.amount} ชิ้น</p>
-      {result.tasks.map((m,i) => {
+      <p>
+        {result.product} {result.amount} ชิ้น
+      </p>
+      {result.tasks.map((m, i) => {
         return (
           <p key={i}>
-           ใช้ <b>{m.name}:</b> {m.amount} ชิ้น หาจาก <b>{m.source}</b>  {m.times} ครั้ง
+            ใช้ <b>{m.name}:</b> {m.amount} ชิ้น หาจาก <b>{m.source}</b>{" "}
+            {m.times} ครั้ง
           </p>
         );
       })}
-      <p className="flex"><img src={getImage("jobstaminaicon.png")} alt="job stamina icon" className="w-5" /><b>Stermina ที่ใช้:</b> {result.staminacost}</p>
+      <p className="flex">
+        <img
+          src={getImage("jobstaminaicon.png")}
+          alt="job stamina icon"
+          className="w-5"
+        />
+        <b>Stermina ที่ใช้:</b> {result.staminacost}
+      </p>
     </div>
   );
 }
